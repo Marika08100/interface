267dcdeb88f0b3a91fd9c48ad10577eb1e0d4f16
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-15 21:28:55 +0300

    Class Square is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Shape shape = new Circle(2);
+        System.out.println(shape.getArea());
+
+        Shape square   = new Square();
 
     }
 }
---
+++ src/Square.java
@@ -0,0 +1,11 @@
+public class Square implements Shape{
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
