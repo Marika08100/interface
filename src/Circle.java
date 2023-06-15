124ab6402f9ca453bd73d98c506999ebd0bd650d
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-15 21:22:33 +0300

    Circle class is implemented

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,12 @@
-public class Circle  {
+public class Circle implements Shape {
 
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
